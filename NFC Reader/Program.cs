b37fa3ab5d703d8849ef5d68f20df4dcae6f6e64
Quentin de POTTER <45800885+QuentinDPT@users.noreply.github.com>
2025-06-15 22:08:51 +0200

    NFC reader project

---
+++ NFC Reader/Program.cs
@@ -0,0 +1,85 @@
+﻿using PCSC.Exceptions;
+using PCSC.Utils;
+using PCSC;
+using PCSC.Monitoring;
+
+static void CheckErr(SCardError err)
+{
+    if (err != SCardError.Success)
+        throw new PCSCException(err,
+            SCardHelper.StringifyError(err));
+}
+
+try
+{
+    var e = ContextFactory.Instance.Establish(SCardScope.System);
+
+    var readers = e.GetReaders();
+
+    var effectiveReader = readers.First();
+    e.Cancel();
+    e.Dispose();
+
+    var monitor = MonitorFactory.Instance.Create(SCardScope.System);
+
+    monitor.CardInserted += (_, _) => Monitor_CardInserted(null, effectiveReader);
+    monitor.CardRemoved += (_, _) => Monitor_CardRemoved();
+
+    monitor.Start(effectiveReader);
+
+    while (true) { }
+}
+catch (PCSCException ex)
+{
+    Console.WriteLine("Ouch: "
+        + ex.Message
+        + " (" + ex.SCardError.ToString() + ")");
+}
+
+void Monitor_CardRemoved()
+{
+    Console.WriteLine("Card removed");
+}
+
+void Monitor_CardInserted(SCardMonitor eerr, string readerName)
+{
+     var context = ContextFactory.Instance.Establish(SCardScope.System);
+
+    // Create a reader object using the existing context
+    SCardReader reader = new SCardReader(context);
+
+    // Connect to the card
+    SCardError err = reader.Connect(readerName,
+        SCardShareMode.Shared,
+        SCardProtocol.T0 | SCardProtocol.T1);
+    CheckErr(err);
+
+    long pioSendPci;
+    switch (reader.ActiveProtocol)
+    {
+        case SCardProtocol.T0:
+            pioSendPci = SCardPCI.T0;
+            break;
+        case SCardProtocol.T1:
+            pioSendPci = SCardPCI.T1;
+            break;
+        default:
+            throw new PCSCException(SCardError.ProtocolMismatch,
+                "Protocol not supported: "
+                + reader.ActiveProtocol.ToString());
+    }
+
+    byte[] pbRecvBuffer = new byte[256];
+
+    // Send test command
+    byte[] getUID = new byte[] { 0xFF, 0xCA, 0x00, 0x00, 0x00 };
+    err = reader.Transmit((nint)pioSendPci, getUID, ref pbRecvBuffer);
+    CheckErr(err);
+
+    Console.Write("New card : ");
+    for (int i = 0; i < pbRecvBuffer.Length; i++)
+        Console.Write("{0:X2} ", pbRecvBuffer[i]);
+    Console.WriteLine();
+
+    context.Release();
+}
---
+++ QMTGroup.DSL.IO/NfcReader.cs
@@ -0,0 +1,29 @@
+﻿using QMTGroup.Core;
+using QMTGroup.DSL.Library;
+
+namespace QMTGroup.DSL.IO;
+
+[DSLNamespace("NFC")]
+public class NfcReader : IDSLLibrary
+{
+    private readonly IWebLogger _webLogger;
+
+    private Action<string>? _action = null;
+
+    public NfcReader(IWebLogger webLogger)
+    {
+        _webLogger = webLogger;
+    }
+
+    [DSLFunction]
+    public void CreateReader()
+    {
+        _action = null;
+    }
+
+    [DSLFunction]
+    public void OnRead(Action<string> action)
+    {
+        _action = action;
+    }
+}
